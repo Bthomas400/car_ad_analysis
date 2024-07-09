40b1df10ca11ce0e92c7f11e8b778c88fd41219f
BThomas400 <brian.eugene47@gmail.com>
2024-07-08 18:57:59 -0700

    Added EDA notebook and Streamlit app

--- app.py
+++ app.py
@@ -5,6 +5,37 @@ import plotly.express as px
 # Load the dataset
 df = pd.read_csv('vehicles_us.csv')
 
+# Remove outliers for model_year and price
+df = df[(df['model_year'] >= df['model_year'].quantile(0.05)) & (df['model_year'] <= df['model_year'].quantile(0.95))]
+df = df[(df['price'] >= df['price'].quantile(0.05)) & (df['price'] <= df['price'].quantile(0.95))]
+
+# Fill missing values based on group medians
+df['model_year'] = df.groupby('model')['model_year'].transform(lambda x: x.fillna(x.median()))
+df['cylinders'] = df.groupby('model')['cylinders'].transform(lambda x: x.fillna(x.median()))
+df['odometer'] = df.groupby(['model_year', 'model'])['odometer'].transform(lambda x: x.fillna(x.mean()))
+
+df['paint_color'] = df['paint_color'].fillna('Unknown')
+
+# Fill any remaining NaN values with 0
+df['odometer'] = df['odometer'].fillna(0)
+
+
+
+# Convert model_year to datetime format
+df['model_year'] = pd.to_datetime(df['model_year'], format='%Y').dt.year
+df['model_year'] = df['model_year'].astype(str)
+
+#Changing data type 
+df['cylinders'] = df['cylinders'].astype('int64')
+
+
+
+#fill `is_4wd` with 'yes' and 'no'
+df['is_4wd']=df['is_4wd'].fillna('no')
+df['is_4wd'] = df['is_4wd'].replace(1, 'yes')
+
+
+
 #create new manufacturer column
 df['manufacturer']=df['model'].apply(lambda x: x.split()[0])
 
